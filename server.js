78b802520e5bede1a8241fb55996d496194f003c
Luka Bartulović <bartul123@outlook.com>
2023-06-17 19:50:08 +0200

    Deploy changes

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 import app from "./app.js";
-console.log("hdawdw");
 import { mongoose } from "mongoose";
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
-console.log(mongoose);
-console.log(PORT);
-console.log(process.env.MONGO_DB_URL);
-mongoose.connect(process.env.MONGO_DB_URL,
+
+mongoose.connect('mongodb+srv://bartul_9:jasamkralj222@cluster0.u0dqa.mongodb.net/?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -18,6 +15,6 @@ mongoose.connect(process.env.MONGO_DB_URL,
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
+db.once("open", async function () {
     console.log("Connected successfully");
 });
